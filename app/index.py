dcf51886a9841fcfcf4289456b9fc0180cea7a39
talvi1 <talha.alvi11@gmail.com>
2021-11-11 05:59:28 +0000

    cpu utilization

--- app/index.py
+++ app/index.py
@@ -2,6 +2,8 @@ from flask import render_template, redirect, url_for, request
 from app import manager
 import boto3
 from datetime import datetime, timedelta
+from operator import itemgetter
+
 
 
 
@@ -18,17 +20,26 @@ def home_page():
 
     cpu = client.get_metric_statistics(
         Period=1 * 60,
-        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
+        StartTime=datetime.utcnow() - timedelta(seconds=30 * 60),
         EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
         MetricName=metric_name,
         Namespace=namespace,  # Unit='Percent',
         Statistics=[statistic],
-        Dimensions=[{'Name': 'InstanceId', 'Value': 'i-05b1e1d83bfbf9abe'}]
+        Dimensions=[{'Name': 'InstanceId', 'Value': 'i-073587260f04b73ce'}]
     )
-    print(cpu)
+  #  print(cpu)
+    cpu_stats = []
+    for point in cpu['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute/60
+        cpu_stats.append([time,point['Average']])
 
+    cpu_stats = sorted(cpu_stats, key=itemgetter(0))
+    print(cpu_stats)
     for instance in instances:
         if (instance.tags[0]['Key'] == 'worker'):
             print(instance.id)
+            print(instance.state)
             #print(instance.tags)
     return render_template('home.html')
