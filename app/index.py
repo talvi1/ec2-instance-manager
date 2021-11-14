a24a070249b68db7da31cc8fc4903678cf2231b8
talvi1 <talha.alvi11@gmail.com>
2021-11-14 03:04:06 +0000

    worker graph

--- app/index.py
+++ app/index.py
@@ -43,3 +43,32 @@ def home_page():
     print(active_instances)
 
     return render_template('home.html')
+
+@manager.route('/workers', methods=['GET'])
+def workers():
+    ec2 = boto3.resource('ec2')
+    instances = ec2.instances.all()
+    client = boto3.client('cloudwatch')
+    metric_name = 'CPUUtilization'
+    namespace = 'AWS/EC2'
+    statistic = 'Average'
+    cpu = client.get_metric_statistics(
+        Period=1 * 60,
+        StartTime=datetime.utcnow() - timedelta(seconds=30 * 60),
+        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName=metric_name,
+        Namespace=namespace,  # Unit='Percent',
+        Statistics=[statistic],
+        Dimensions=[{'Name': 'InstanceId', 'Value': 'i-073587260f04b73ce'}]
+    )
+  #  print(cpu)
+    cpu_stats = []
+    for point in cpu['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute/60
+        cpu_stats.append([time,point['Average']])
+
+    cpu_stats = sorted(cpu_stats, key=itemgetter(0))
+    print(len(cpu_stats))
+    return render_template('workers.html', value=cpu_stats)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!venv/bin/python
 from app import manager
 
-manager.run(host='0.0.0.0')
+manager.run(debug=True, host='0.0.0.0')
